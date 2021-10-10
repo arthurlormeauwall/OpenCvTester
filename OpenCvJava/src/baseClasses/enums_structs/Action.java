68298162304c0a7ff703bdee2163081db1175f44
arthurlormeauwall <arthurlormeau@gmail.com>
2021-10-10 16:05:22 +0200

    separates testing

---
+++ OpenCvJava/src/baseClasses/enums_structs/Action.java
@@ -0,0 +1,10 @@
+package baseClasses.enums_structs;
+
+import java.util.Stack;
+import baseClasses.Id;
+
+public class Action {
+	public Stack<Id> id;
+	public Functionalities whatToDo;
+	public Parameters parameters;
+}
