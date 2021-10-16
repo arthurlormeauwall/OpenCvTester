cfc9a2ab637c2afd44eab2c5092868e550de9dd6
arthurlormeauwall <arthurlormeau@gmail.com>
2021-10-16 16:29:42 +0200

    adjust main function

--- OpenCvJava/Example/main/Main.java
+++ OpenCvJava/Example/main/Main.java
@@ -8,42 +8,42 @@ import fakeGui.FakeGui;
 public class Main 
 {
     public static void main(String[] args){
-
-			FakeGui fakeGui= new FakeGui("assets/20210717_203824.jpg"); 
-    		fakeGui.addAlgorithm(new MultBgrControl());
     		
-    		Stack<Integer> controlIndexInDataBase= new Stack<Integer>();
-    		controlIndexInDataBase.push(1);
-    		fakeGui.addLayer(0, controlIndexInDataBase);
-    		fakeGui.store();
-    		fakeGui.play();
+		FakeGui fakeGui= new FakeGui("assets/20210717_203824.jpg"); 
+		fakeGui.addAlgorithm(new MultBgrControl());
     		
-    		fakeGui.setBypass(0, 0, false);
-    		fakeGui.play();
-    		fakeGui.undo();
-    		fakeGui.undo();
-    		fakeGui.undo();
-    		fakeGui.undo();
+		Stack<Integer> controlIndexInDataBase= new Stack<Integer>();
+		controlIndexInDataBase.push(1);
+		fakeGui.addLayer(0, controlIndexInDataBase);
+		fakeGui.store();
+		fakeGui.play();
+    		
+		fakeGui.setBypass(0, 0, false);
+		fakeGui.play();
+		fakeGui.undo();
+		fakeGui.undo();
+		fakeGui.undo();
+		fakeGui.undo();
   
-    		fakeGui.play();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.redo();
-    		fakeGui.play();
+		fakeGui.play();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.redo();
+		fakeGui.play();
     		
-    		Stack<Float> floatParameters=new Stack<Float>();
-    		floatParameters.push(0.6f);
-    		floatParameters.push(2f);
-    		floatParameters.push(0.9f);
+		Stack<Float> floatParameters=new Stack<Float>();
+		floatParameters.push(0.6f);
+		floatParameters.push(2f);
+		floatParameters.push(0.9f);
     		
-    		fakeGui.setParameters(0, 0, floatParameters);	
-    		fakeGui.store();	
-    		fakeGui.play();
+		fakeGui.setParameters(0, 0, floatParameters);	
+		fakeGui.store();	
+		fakeGui.play();
     }
 }
