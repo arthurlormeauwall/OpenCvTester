d7c5833c74fd1e178c50e10af2c596ab249bfe67
arthurlormeauwall <arthurlormeau@gmail.com>
2021-10-18 09:10:12 +0200

    clean main

--- OpenCvJava/Example/main/Main.java
+++ OpenCvJava/Example/main/Main.java
@@ -16,6 +16,7 @@ public class Main
     		
 		Stack<Integer> controlIndexInDataBase= new Stack<Integer>();
 		controlIndexInDataBase.push(0);
+		controlIndexInDataBase.push(1);
 		fakeGui.addLayer(0, controlIndexInDataBase);
 		fakeGui.setBypass(0, 0, false);
 		
@@ -25,43 +26,12 @@ public class Main
 		floatParameters.push(0.9f);
     		
 		fakeGui.setParameters(0, 0, floatParameters);	
-		floatParameters.clear();
 		
-		floatParameters.push(1f);
-		floatParameters.push(0.3f);
-		floatParameters.push(1.6f);
-		fakeGui.setParameters(0, 0, floatParameters);	
-		floatParameters.clear();
-		
-		floatParameters.push(1.2f);
-		floatParameters.push(1.6f);
-		floatParameters.push(0.9f);
-		fakeGui.setParameters(0, 0, floatParameters);	
-		floatParameters.clear();
-		
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
 		fakeGui.undo();
 		fakeGui.undo();
 		fakeGui.undo();
 		fakeGui.redo();
-		
-		controlIndexInDataBase.clear();
-		controlIndexInDataBase.push(0);
-		fakeGui.addLayer(1, controlIndexInDataBase);
-		fakeGui.setBypass(1, 0, false);
-		
 		fakeGui.redo();
 		fakeGui.redo();
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
-		fakeGui.undo();
     }
 }
