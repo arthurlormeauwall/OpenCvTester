76af7205b56eb330a33eb7799f6582161f8aa0ab
arthurlormeauwall <arthurlormeau@gmail.com>
2021-11-15 14:55:49 +0100

    re adjusting bgrmultfilter to correct defaultValues

--- OpenCvJava/Example/userFilters/BlueGreenRedMultiplierFilter.java
+++ OpenCvJava/Example/userFilters/BlueGreenRedMultiplierFilter.java
@@ -10,9 +10,9 @@ public class BlueGreenRedMultiplierFilter extends FilterControlledByFloat
 
 	public void setParameterFlags() {	
 
-		addParameterFlag("BlueMult", 0.5f, 1f);
-		addParameterFlag("GreenMult", 0.5f, 1f);
-		addParameterFlag("RedMult", 0.5f, 1f);
+		addParameterFlag("BlueMult", 1f, 1f);
+		addParameterFlag("GreenMult", 1f, 1f);
+		addParameterFlag("RedMult", 1f, 1f);
 		
 		setFilterName("BGR Multiplier");
 	}
