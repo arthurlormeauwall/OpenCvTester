8a78e5dbbc22834645c1948744fda49eb6f53082
Arthur Lormeau Wall <arthurlormeau@gmail.com>
2022-01-05 22:20:32 +0100

    add junit to maven dependencies

---
+++ src/test/java/com/opencvtester/app/AppTest.java
@@ -0,0 +1,13 @@
+package com.opencvtester.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
