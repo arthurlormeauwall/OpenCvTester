3451f98e0873292e9af012a4bc31c0ba6f91f12d
Arthur Lormeau Wall <arthurlormeau@gmail.com>
2022-01-13 08:08:05 +0100

    Test FilterControlledByFloat class

---
+++ src/test/java/com/opencvtester/baseClasses/filter/DummyFilter.java
@@ -0,0 +1,26 @@
+package com.opencvtester.baseClasses.filter;
+
+public class DummyFilter extends FilterControlledByFloat{
+
+	@SuppressWarnings("unused")
+	private String log;
+	
+	@Override
+	public FilterControlledByFloat createNew() {
+		
+		return new DummyFilter();
+	}
+
+	@Override
+	public void setParameterFlags() {
+		addParameterFlag("BlueMult", 1f, 1f, 130);
+		addParameterFlag("GreenMult", 1f, 1f, 130);
+		addParameterFlag("RedMult", 1f, 1f, 130);	
+	}
+
+	@Override
+	public void execute() {
+		log = "dymmy filter is executing";	
+	}
+
+}
--- src/test/java/com/opencvtester/baseClasses/filter/FilterControlledByFloatTest.java
+++ src/test/java/com/opencvtester/baseClasses/filter/FilterControlledByFloatTest.java
@@ -2,38 +2,80 @@ package com.opencvtester.baseClasses.filter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.LinkedHashMap;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class FilterControlledByFloatTest {
+import com.opencvtester.baseClasses.frame.Frame;
 
+class FilterControlledByFloatTest {
+	FilterControlledByFloat filter;
+	private Float a=1.2f, b=1.6f, c=2.6f;
+	
+	@BeforeEach
+	public void setup() {
+		filter= new DummyFilter();
+	}
+	
 	@Test
 	void testAddParameterFlag() {
-		
+		LinkedHashMap<String, Float> parameters= filter.getParameters();
+		assertEquals(1f, parameters.get("BlueMult"));
+		assertEquals(1f, parameters.get("GreenMult"));
+		assertEquals(1f, parameters.get("RedMult"));
 	}
 
 	@Test
 	void testReset() {
-		
+		testSetParameter();
+		filter.reset();
+		LinkedHashMap<String, Float> parameters= filter.getParameters();
+		assertEquals(1f, parameters.get("BlueMult"));
+		assertEquals(1f, parameters.get("GreenMult"));
+		assertEquals(1f, parameters.get("RedMult"));
 	}
 
 	@Test
 	void testSetParameter() {
-		
+		filter.setParameter("BlueMult", a);
+		filter.setParameter("GreenMult", b);
+		filter.setParameter("RedMult", c);
+		LinkedHashMap<String, Float> parameters= filter.getParameters();
+		assertEquals(a, parameters.get("BlueMult"));
+		assertEquals(b, parameters.get("GreenMult"));
+		assertEquals(c, parameters.get("RedMult"));	
 	}
 
 	@Test
 	void testSetAllParameters() {
-	
+		LinkedHashMap<String, Float> parameters= new LinkedHashMap<String, Float>();
+		parameters.put("BlueMult", a);
+		parameters.put("GreenMult", b);
+		parameters.put("RedMult", c);
+		
+		filter.setAllParameters(parameters);
+		assertEquals(a, parameters.get("BlueMult"));
+		assertEquals(b, parameters.get("GreenMult"));
+		assertEquals(c, parameters.get("RedMult"));		
 	}
 
 	@Test
 	void testSetFrameIn() {
-	
+		Frame frameIn= new Frame(10,10,127);
+		
+		filter.setFrameIn(frameIn);
+		
+		assertTrue(frameIn.compareTo(filter.getFrameIn())==0);
 	}
 
 	@Test
 	void testSetFrameOut() {
-
+		Frame frameOut= new Frame(10,10,127);
+		
+		filter.setFrameOut(frameOut);
+		
+		assertTrue(frameOut.compareTo(filter.getFrameOut())==0);
 	}
 
 }
